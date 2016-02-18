176ed3c069f7c603de10e5e1f1daf23fe2e32b37
Bernhard Bablok <bablokb@gmx.de>
2016-02-18 11:13:30 +0100

    import all classes

---
+++ src/ediplug/__init__.py
@@ -0,0 +1,6 @@
+from PlugFinder import PlugFinder as PlugFinder
+from Plug import Plug as Plug
+from Schedule import Schedule as Schedule
+from SP1101W import SP1101W as SP1101W
+from SP2101W import SP2101W as SP2101W
+from TPoint import TPoint as TPoint

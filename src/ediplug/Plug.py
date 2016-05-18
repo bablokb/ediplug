64f953f7fa67f3b4ca169759c18c41857734b3ef
Sven Möller <m0ellemeister@nichthelfer.de>
2016-05-18 23:39:47 +0200

    reworked Plug.py partly making it Python 3.x compatible.

--- src/ediplug/Plug.py
+++ src/ediplug/Plug.py
@@ -22,235 +22,239 @@ from xml.dom.minidom import parseString
 from Schedule import Schedule as Schedule
 from TPoint import TPoint as TPoint
 
+
 class Plug(object):
-  """Base class of supported Edimax Plugs"""
+    """Base class of supported Edimax Plugs"""
 
-  # initialize Plug object   -------------------------------------------------
+    # initialize Plug object   -------------------------------------------------
 
-  def __init__(self,ip,port=10000,user='admin',password='1234'):
-    self.__url = 'http://%s:%s/smartplug.cgi' % (ip,port)
-    self.__cred = (user,password)
-    self.__info = None
-    self.__debug = True if os.getenv('DEBUG') is not None else False
+    def __init__(self, ip, port=10000, user='admin', password='1234'):
+        self.__url = 'http://%s:%s/smartplug.cgi' % (ip, port)
+        self.__cred = (user, password)
+        self.__info = None
+        self.__debug = True if os.getenv('DEBUG') is not None else False
 
-    if self.__debug:
-      sys.stderr.write("url: %s\n" % self.__url)
+        if self.__debug:
+            sys.stderr.write("url: %s\n" % self.__url)
 
-  # string representation of plug   -------------------------------------------
-  
-  def __str__(self):
-    return "Plugtype: %s" % type(self)
+    # string representation of plug   -------------------------------------------
 
-  # technical representation of plug   ----------------------------------------
-  
-  def __repr__(self):
-    return "Type: %s\nURL: %s" % (type(self),self.__url)
-    
-  # create XML command document   --------------------------------------------
-  
-  def _getXML(self,cmdType):
-    doc = getDOMImplementation().createDocument(None, "SMARTPLUG", None)
-    doc.documentElement.setAttribute("id", "edimax")
-    
-    cmdElem = doc.createElement("CMD")
-    cmdElem.setAttribute("id", cmdType)
-    doc.documentElement.appendChild(cmdElem)
-    return (doc,cmdElem)
+    def __str__(self):
+        return "Plugtype: %s" % type(self)
 
-  # post request and return DOM   ---------------------------------------------
-  
-  def _postCmd(self,doc):
-    if self.__debug:
-      sys.stderr.write(doc.toprettyxml())
-      
-    res = req.post(self.__url, auth=self.__cred, files={'file': doc.toxml()})
-    if self.__debug:
-      print res
-    if res.status_code == req.codes.ok:
-      result = parseString(res.text)
-      if self.__debug:
-        sys.stderr.write(result.toprettyxml())
-      return result
-    else:
-      # TODO: throw some exception??
-      pass
+    # technical representation of plug   ----------------------------------------
 
-  # execute generic command   ------------------------------------------------
+    def __repr__(self):
+        return "Type: %s\nURL: %s" % (type(self), self.__url)
 
-  def _execCommand(self,cmdType,tag,value=None):
-    doc, cmdElem = self._getXML(cmdType)
-    childElem = doc.createElement(tag)
-    cmdElem.appendChild(childElem)
-    if value is not None:
-      childElem.appendChild(doc.createTextNode(value))
-    return self._postCmd(doc)
+    # create XML command document   --------------------------------------------
 
-  # return url of plug   ------------------------------------------------------
+    def _getXML(self, cmdType):
+        doc = getDOMImplementation().createDocument(None, "SMARTPLUG", None)
+        doc.documentElement.setAttribute("id", "edimax")
 
-  def getUrl(self):
-    return self.__url
-  
-  # parse result of command   -------------------------------------------------
+        cmdElem = doc.createElement("CMD")
+        cmdElem.setAttribute("id", cmdType)
+        doc.documentElement.appendChild(cmdElem)
+        return (doc, cmdElem)
 
-  def _parseResult(self,dom):
-    value = dom.getElementsByTagName("CMD")[0].firstChild.nodeValue
-    return True if value == 'OK' else False
-    
-  # query name and type of plug   --------------------------------------------
+    # post request and return DOM   ---------------------------------------------
 
-  def getNameAndType(self):
-    info = self.getSysInfo()
-    return (info['Device.System.Name'],info['Run.Model'])
+    def _postCmd(self, doc):
+        if self.__debug:
+            sys.stderr.write(doc.toprettyxml())
 
-  # query system-info   ------------------------------------------------------
+        try:
+            res = req.post(self.__url, auth=self.__cred, files={'file': doc.toxml()})
+            if self.__debug:
+                print(res)
+            if res.status_code == req.codes.ok:
+                result = parseString(res.text)
+                if self.__debug:
+                    sys.stderr.write(result.toprettyxml())
+                return result
+            else:
+                # TODO: throw some exception??
+                pass
+        except req.exceptions.ConnectionError as e:
+            return e
 
-  def getSysInfo(self):
-    if self.__info is not None:
-      return self.__info
-    
-    dom = self._execCommand('get',"SYSTEM_INFO")
-    info = {}
-    tags = dom.getElementsByTagName("SYSTEM_INFO")[0].childNodes
-    for tag in tags:
-      if tag.tagName == "SUPPORT":
-        for supportTag in tag.childNodes:
-          info[supportTag.tagName] = supportTag.firstChild.nodeValue
-      else:
-        if tag.hasChildNodes():
-          info[tag.tagName] = tag.firstChild.nodeValue
-    self.__info = info
-    return info
+    # execute generic command   ------------------------------------------------
 
-  # query power-state   ------------------------------------------------------
+    def _execCommand(self, cmdType, tag, value=None):
+        doc, cmdElem = self._getXML(cmdType)
+        childElem = doc.createElement(tag)
+        cmdElem.appendChild(childElem)
+        if value is not None:
+            childElem.appendChild(doc.createTextNode(value))
+        return self._postCmd(doc)
 
-  def getPowerState(self):
-    dom = self._execCommand('get',"Device.System.Power.State")
-    value = dom.getElementsByTagName("Device.System.Power.State")[0].\
-                                                          firstChild.nodeValue
-    return True if value == 'ON' else False
+    # return url of plug   ------------------------------------------------------
 
-  # set power-state   ---------------------------------------------------------
+    def getUrl(self):
+        return self.__url
 
-  def setPowerState(self,active=True):
-    value = 'ON' if active else 'OFF'
-    dom = self._execCommand('setup',"Device.System.Power.State",value)
-    return self._parseResult(dom)
+    # parse result of command   -------------------------------------------------
 
-  # query schedule   ----------------------------------------------------------
+    def _parseResult(self, dom):
+        value = dom.getElementsByTagName("CMD")[0].firstChild.nodeValue
+        return True if value == 'OK' else False
 
-  def getSchedule(self,schedule=None,getDom=False):
-    if schedule is None:
-      schedule = Schedule()
-    days = [ i for i in range(7) ]
+    # query name and type of plug   --------------------------------------------
 
-    # build command-xml ...
-    doc, cmdElem = self._getXML('get')
-    schedElem = doc.createElement("SCHEDULE")
-    cmdElem.appendChild(schedElem)
-    
-    if not getDom:      # return full information
-      for d in days:
-        dayElem = doc.createElement("Device.System.Power.Schedule."+str(d))
-        schedElem.appendChild(dayElem)
+    def getNameAndType(self):
+        info = self.getSysInfo()
+        return (info['Device.System.Name'], info['Run.Model'])
 
-    # ... post and parse the result
-    dom = self._postCmd(doc)
-    if getDom:
-      return dom
-    
-    tags = dom.getElementsByTagName("SCHEDULE")[0].childNodes
-    for tag in tags:
-      name = tag.tagName
-      if name.find('List') > 0:
-        continue
-      day = name.split('.')[-1]
-      value = tag.firstChild.nodeValue
-      schedule.fromTransport(value,int(day))
-    return schedule
+    # query system-info   ------------------------------------------------------
 
-  # set schedule (for a given day)   ------------------------------------------
+    def getSysInfo(self):
+        if self.__info is not None:
+            return self.__info
 
-  def setSchedule(self,schedule,day=None):
-    doc, cmdElem = self._getXML('setup')
-    schedElem = doc.createElement("SCHEDULE")
-    cmdElem.appendChild(schedElem)
+        dom = self._execCommand('get', "SYSTEM_INFO")
+        info = {}
+        tags = dom.getElementsByTagName("SYSTEM_INFO")[0].childNodes
+        for tag in tags:
+            if tag.tagName == "SUPPORT":
+                for supportTag in tag.childNodes:
+                    info[supportTag.tagName] = supportTag.firstChild.nodeValue
+            else:
+                if tag.hasChildNodes():
+                    info[tag.tagName] = tag.firstChild.nodeValue
+        self.__info = info
+        return info
 
-    # iterate over all days (either given day or all days)
-    # and add XML-node to command-document
-    
-    days = [day] if day is not None else [ i for i in range(7) ]
-    for d in days:
-      dayElem = doc.createElement("Device.System.Power.Schedule."+str(d))
-      dayElem.setAttribute("value","ON")
-      dayElem.appendChild(doc.createTextNode(schedule.toTransport(d)))
-      schedElem.appendChild(dayElem)
+    # query power-state   ------------------------------------------------------
 
-      listElem = doc.createElement("Device.System.Power.Schedule."+str(d)+".List")
-      listElem.appendChild(doc.createTextNode(schedule.getTransportSwitchList(d)))
-      schedElem.appendChild(listElem)
+    def getPowerState(self):
+        dom = self._execCommand('get', "Device.System.Power.State")
+        value = dom.getElementsByTagName("Device.System.Power.State")[0]. \
+            firstChild.nodeValue
+        return True if value == 'ON' else False
 
-    return self._parseResult(self._postCmd(doc))
+    # set power-state   ---------------------------------------------------------
 
-  # set state for a given time-range   ----------------------------------------
-  # (this only changes the schedule in the given range)
+    def setPowerState(self, active=True):
+        value = 'ON' if active else 'OFF'
+        dom = self._execCommand('setup', "Device.System.Power.State", value)
+        return self._parseResult(dom)
 
-  def setState(self,start,end,active=True):
-    sched = self.getSchedule()          # query current schedule
-    sched.setState(start,end,active)    # set value in given range
-    return self.setSchedule(sched)      # write schedule back to plug
+    # query schedule   ----------------------------------------------------------
 
-  # configure (exclusive) time in status ON/OFF   -----------------------------
+    def getSchedule(self, schedule=None, getDom=False):
+        if schedule is None:
+            schedule = Schedule()
+        days = [i for i in range(7)]
 
-  def setExclusiveState(self,start,end,active=True):
-    sched = Schedule(not active)            # all ON or OFF schedule
-    sched.setState(start,end,active)        # time range in status OFF/ON
-    return self.setSchedule(sched)          # write schedule to plug
-  
-  # clear schedule   ----------------------------------------------------------
+        # build command-xml ...
+        doc, cmdElem = self._getXML('get')
+        schedElem = doc.createElement("SCHEDULE")
+        cmdElem.appendChild(schedElem)
 
-  def clear(self,active=True):
-    sched = Schedule(active)
-    self.setSchedule(sched)
+        if not getDom:  # return full information
+            for d in days:
+                dayElem = doc.createElement("Device.System.Power.Schedule." + str(d))
+                schedElem.appendChild(dayElem)
 
-  
+        # ... post and parse the result
+        dom = self._postCmd(doc)
+        if getDom:
+            return dom
+
+        tags = dom.getElementsByTagName("SCHEDULE")[0].childNodes
+        for tag in tags:
+            name = tag.tagName
+            if name.find('List') > 0:
+                continue
+            day = name.split('.')[-1]
+            value = tag.firstChild.nodeValue
+            schedule.fromTransport(value, int(day))
+        return schedule
+
+    # set schedule (for a given day)   ------------------------------------------
+
+    def setSchedule(self, schedule, day=None):
+        doc, cmdElem = self._getXML('setup')
+        schedElem = doc.createElement("SCHEDULE")
+        cmdElem.appendChild(schedElem)
+
+        # iterate over all days (either given day or all days)
+        # and add XML-node to command-document
+
+        days = [day] if day is not None else [i for i in range(7)]
+        for d in days:
+            dayElem = doc.createElement("Device.System.Power.Schedule." + str(d))
+            dayElem.setAttribute("value", "ON")
+            dayElem.appendChild(doc.createTextNode(schedule.toTransport(d)))
+            schedElem.appendChild(dayElem)
+
+            listElem = doc.createElement("Device.System.Power.Schedule." + str(d) + ".List")
+            listElem.appendChild(doc.createTextNode(schedule.getTransportSwitchList(d)))
+            schedElem.appendChild(listElem)
+
+        return self._parseResult(self._postCmd(doc))
+
+    # set state for a given time-range   ----------------------------------------
+    # (this only changes the schedule in the given range)
+
+    def setState(self, start, end, active=True):
+        sched = self.getSchedule()  # query current schedule
+        sched.setState(start, end, active)  # set value in given range
+        return self.setSchedule(sched)  # write schedule back to plug
+
+    # configure (exclusive) time in status ON/OFF   -----------------------------
+
+    def setExclusiveState(self, start, end, active=True):
+        sched = Schedule(not active)  # all ON or OFF schedule
+        sched.setState(start, end, active)  # time range in status OFF/ON
+        return self.setSchedule(sched)  # write schedule to plug
+
+    # clear schedule   ----------------------------------------------------------
+
+    def clear(self, active=True):
+        sched = Schedule(active)
+        self.setSchedule(sched)
+
+
 # -----------------------------------------------------------------------------
 # test program   --------------------------------------------------------------
 
 if __name__ == "__main__":
-  try:
-    if len(sys.argv) > 2:
-      plug = Plug(sys.argv[1],password=sys.argv[2])
+    try:
+        if len(sys.argv) > 2:
+            plug = Plug(sys.argv[1], password=sys.argv[2])
 
-      # query power state
-      state = plug.getPowerState()
-      print "Power state is active: %s" % state
+            # query power state
+            state = plug.getPowerState()
+            print("Power state is active: {0}".format(state))
 
-      # set power state
-      result = plug.setPowerState(len(sys.argv) > 3)
-      print "Setting power state... success?:", result
+            # set power state
+            result = plug.setPowerState(len(sys.argv) > 3)
+            print("Setting power state... success?: {0}".format(result))
 
-      # query current schedule
-      print "Current schedule:"
-      print plug.getSchedule()
+            # query current schedule
+            print("Current schedule:")
+            print(plug.getSchedule())
 
-      sched = Schedule(False)
-      sched.setState(TPoint(TPoint.MON,0,0),TPoint(TPoint.TUE,0,0),True)
-      print "input schedule:"
-      print sched
-      print "Setting schedule... success:", plug.setSchedule(sched)
-      print "Output schedule from plug:"
-      #print plug.getSchedule(getDom=True).toprettyxml()
-      print plug.getSchedule()
+            sched = Schedule(False)
+            sched.setState(TPoint(TPoint.MON, 0, 0), TPoint(TPoint.TUE, 0, 0), True)
+            print("input schedule:")
+            print(sched)
+            print("Setting schedule... success:".format(plug.setSchedule(sched)))
+            print("Output schedule from plug:")
+            # print plug.getSchedule(getDom=True).toprettyxml()
+            print(plug.getSchedule())
 
-      #print \
-      #  plug.setState(TPoint(TPoint.FRI,0,0),TPoint(TPoint.SAT,0,0),False).\
-      #  toprettyxml()
+            # print \
+            #  plug.setState(TPoint(TPoint.FRI,0,0),TPoint(TPoint.SAT,0,0),False).\
+            #  toprettyxml()
 
-      #now = TPoint.now()
-      #now = now.createAfter(1,0,0)
-      #then = now.createAfter(0,3,0)
-      #print plug.setExclusiveState(now,then,False).toprettyxml()
-    else:
-      print "please pass host/ip and password as argument"
-  except Exception as e:
-    print e
+            # now = TPoint.now()
+            # now = now.createAfter(1,0,0)
+            # then = now.createAfter(0,3,0)
+            # print plug.setExclusiveState(now,then,False).toprettyxml()
+        else:
+            print("please pass host/ip and password as argument")
+    except Exception as e:
+        print(e)
